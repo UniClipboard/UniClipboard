7d213e3770ee7cdcc1632cb0e9b7d94a92dd5847
mkdir700 <mkdir700@gmail.com>
2026-08-18 19:46:45 +0800

    fix(quick-panel): disable production developer tools

--- src/quick-panel/main.tsx
+++ src/quick-panel/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import '@/i18n'
+import { initializeWebviewContextMenu } from '@/lib/webview-context-menu'
 import { initializeWindowUi } from '@/lib/window-ui'
 import '@/lib/wdio-test-bridge'
 import { store } from '@/store'
@@ -9,6 +10,7 @@ import '@/styles/globals.css'
 import QuickPanelApp from './QuickPanelApp'
 
 initializeWindowUi()
+initializeWebviewContextMenu()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
